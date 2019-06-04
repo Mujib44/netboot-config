4a8050dccc4b0dffd56253c49cfa57ff65cc1f89
Andreas Wuerl <andreas.wuerl@uniscon.com>
2019-06-04 10:02:14 +0200

    version 0.1.2 fixed argument

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # coding=utf-8
+import glob
+
 import setuptools
 
 setuptools.setup(
     name="netboot-config",
-    version="0.1",
+    version="0.1.2",
     author="Andreas Würl",
     author_email="andreas.wuerl@uniscon.com",
     description="Generator for KIWI based netboot config files",
@@ -11,6 +13,7 @@ setuptools.setup(
     #    install_requires=['configparser==3.5.0','Flask==1.0.2','jsontool==0.2.1','psutil==5.6.1','linux-utils==0.6','pyparsing==2.3.1','requests==2.21.0','rfc5424-logging-handler==1.3.0','Werkzeug==0.14.1', 'netaddr==0.7.19','netifaces==0.10.9'],
     #    url="https://gitlab.uniscon-rnd.de/sealed-platform/sealed-control-plane/tree/develop/SCSIVolumeManagerAPI",
     packages=["netboot_config"],
+    scripts=["netboot-config"],
     classifiers=[
         'Development Status :: 3 - Alpha',
 
