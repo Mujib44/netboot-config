d9fb986f4910398d739fa06980a3118f9aa7ad9d
Andreas Wuerl <andreas.wuerl@uniscon.com>
2019-06-04 10:26:10 +0200

    fix config file handling

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="netboot-config",
-    version="0.1.2",
+    version="0.1.3",
     author="Andreas Würl",
     author_email="andreas.wuerl@uniscon.com",
     description="Generator for KIWI based netboot config files",
