30170d6cf746757d7603acbd4507ebb381e5cc0f
Andreas Wuerl <andreas.wuerl@uniscon.com>
2019-06-05 12:07:48 +0200

    add default entries for hosts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="netboot-config",
-    version="0.1.7",
+    version="0.1.8",
     author="Andreas Würl",
     author_email="andreas.wuerl@uniscon.com",
     description="Generator for KIWI based netboot config files",
